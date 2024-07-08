b14498ed86c07af4cbd8a7b446840ab834256dd7
YulikK <yulikpoch@gmail.com>
2024-07-08 11:03:25 +0200

    fix: add proxy for api, add lint-staged config file, change husky config

--- src/common/constant.tsx
+++ src/common/constant.tsx
@@ -1,7 +1,9 @@
 export const GUID_STORE = 'RSS_YULIK_8600_3821_3746';
-export const URL_SEARCH_API = 'https://api.themoviedb.org/3/search/movie';
-export const URL_API = 'https://api.themoviedb.org/3/movie/top_rated';
-export const URL_POSTER = 'https://image.tmdb.org/t/p/w300';
+export const URL_SEARCH_API =
+  'https://node-proxy-bkj4.onrender.com/api/3/search/movie';
+export const URL_API =
+  'https://node-proxy-bkj4.onrender.com/api/3/movie/top_rated';
+export const URL_POSTER = 'https://node-proxy-bkj4.onrender.com/img/t/p/w300';
 export const TOKEN =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTMxYTdhYzY0ZmNlOGQzN2RlOGQxZDcwMGZkNjJhZSIsIm5iZiI6MTcyMDA5NzE4NS4zODc5NzYsInN1YiI6IjY2ODY5N2E5YzJjY2Y5Nzg0ZjI1OTk5NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gjma8G8TLmaG68y4mybcZDKv3PBmvG3HA0bO3kfqOBA';
 export const API_KEY = '3131a7ac64fce8d37de8d1d700fd62ae';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import sassDts from 'vite-plugin-sass-dts';
+// import sassDts from 'vite-plugin-sass-dts';
 
 export default defineConfig({
-  plugins: [react(), sassDts()],
+  plugins: [react()],
 });
