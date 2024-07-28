56db05e419c859864ec8652f1c0cbc729ebfde28
YulikK <yulikpoch@gmail.com>
2024-07-28 22:11:18 +0200

    feat: change url for cross-check

--- src/common/constant.tsx
+++ src/common/constant.tsx
@@ -2,16 +2,16 @@ export const GUID_STORE = 'RSS_YULIK_8600_3821_3746';
 
 // URL for cross-check
 //
-// export const URL_SEARCH_API =
-//   'https://node-proxy-bkj4.onrender.com/api/3/search/movie';
-// export const URL_API = 'https://node-proxy-bkj4.onrender.com/api/3/movie';
-// export const URL_POSTER = 'https://node-proxy-bkj4.onrender.com/img/t/p/w300';
+export const URL_SEARCH_API =
+  'https://node-proxy-bkj4.onrender.com/api/3/search/movie';
+export const URL_API = 'https://node-proxy-bkj4.onrender.com/api/3/movie';
+export const URL_POSTER = 'https://node-proxy-bkj4.onrender.com/img/t/p/w300';
 
 // URL for develop
 //
-export const URL_SEARCH_API = 'https://api.themoviedb.org/3/search/movie';
-export const URL_API = 'https://api.themoviedb.org/3/movie';
-export const URL_POSTER = 'https://image.tmdb.org/t/p/w300';
+// export const URL_SEARCH_API = 'https://api.themoviedb.org/3/search/movie';
+// export const URL_API = 'https://api.themoviedb.org/3/movie';
+// export const URL_POSTER = 'https://image.tmdb.org/t/p/w300';
 
 export const TOKEN =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTMxYTdhYzY0ZmNlOGQzN2RlOGQxZDcwMGZkNjJhZSIsIm5iZiI6MTcyMDA5NzE4NS4zODc5NzYsInN1YiI6IjY2ODY5N2E5YzJjY2Y5Nzg0ZjI1OTk5NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gjma8G8TLmaG68y4mybcZDKv3PBmvG3HA0bO3kfqOBA';
